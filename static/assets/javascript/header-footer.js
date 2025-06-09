47cb97cec7ca3193c2260ee0e587840c7cb25059
Swarnim1812 <swarnimraj1812@gmail.com>
2025-06-10 01:14:16 +0530

    crime locator page

--- static/assets/javascript/header-footer.js
+++ static/assets/javascript/header-footer.js
@@ -1,3 +1,4 @@
+//<a href="feed.html" class="btn btn-primary text-dark mr-2"><i class="fas fa-newspaper"></i>&nbsp CrimeFeed</a>
 class MyHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML =
@@ -23,15 +24,14 @@ class MyHeader extends HTMLElement {
       <a href="crime-charts.html" class="btn btn-primary text-dark mr-2"><i class="fas fa-globe-asia"></i>&nbsp CrimeCharts</a>
       <a href="crime-predictor.html" class="btn btn-primary text-dark mr-2"><i class="fas fa-robot"></i>&nbsp CrimePredictor</a>  
       <a href="crime-locator.html" class="btn btn-primary text-dark mr-2"><i class="fas fa-map-marker-alt"></i> &nbsp CrimeLocator</a>
-      <a href="feed.html" class="btn btn-primary text-dark mr-2"><i class="fas fa-newspaper"></i>&nbsp CrimeFeed</a>
       <a href="about.html" class="btn btn-primary text-dark mr-2"><i class="fas fa-user-secret"></i>&nbsp About Us</a>
       <a href="help-page.html" class="btn btn-primary text-dark mr-2"><i class="fas fa-info-circle"></i>&nbsp Help</a>
-    </div>
-  </div>
-
-  <a href="#" class="btn-emoji" onclick="audio()">
-    <i class="far fa-grin-tongue"></i>
-  </a>
+      </div>
+      </div>
+      
+      <a href="#" class="btn-emoji" onclick="audio()">
+      <i class="far fa-grin-tongue"></i>
+      </a>
 
 
   <div id="txt"> </div>
